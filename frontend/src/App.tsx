4627dd9e09f68fbd23ed99dcb294b2d85b090f16
miensoap <miensop51@gmail.com>
2024-10-27 13:44:47 +0900

    feat. React-query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  QueryClientProvider,
+  QueryClient,
+  useQuery,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-function App() {
+const queryClient = new QueryClient();
+const getHiMessage = async () => {
+  return await fetch('http://localhost:8080').then((res) => res.text());
+};
+
+function AppContent() {
+  const { data: hiMessage } = useQuery({
+    queryKey: ['hi'],
+    queryFn: getHiMessage,
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>{hiMessage ?? 'no message'}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,6 +34,15 @@ function App() {
         </a>
       </header>
     </div>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppContent />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
