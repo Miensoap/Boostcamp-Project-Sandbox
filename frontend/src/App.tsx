1cce0365c629fada8cbc3600d52537970567e788
miensoap <miensop51@gmail.com>
2024-11-01 18:14:43 +0900

    chore. FE 초기상태

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,47 +1,16 @@
 import React from 'react';
 import './App.css';
-import {
-  QueryClientProvider,
-  QueryClient,
-  useQuery,
-} from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import useStore from './store';
-
-const queryClient = new QueryClient();
-const getHiMessage = async () => {
-  return await fetch('http://localhost:8080').then((res) => res.text());
-};
 
 function AppContent() {
-  const { data: hiMessage } = useQuery({
-    queryKey: ['hi'],
-    queryFn: getHiMessage,
-  });
-
-  const { count, increase, decrease } = useStore();
-
   return (
     <div className="App">
-      <p className={'text-xl text-blue-600 underline'}>
-        {hiMessage ?? 'no message'}
-      </p>
-      <div className="counter">
-        <p className="text-lg">Count: {count}</p>
-        <button onClick={increase}>Increase</button>
-        <button onClick={decrease}>Decrease</button>
-      </div>
+      <p className={'text-xl text-blue-600 underline'}>Daily Load</p>
     </div>
   );
 }
 
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <AppContent />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
-  );
+  return <AppContent />;
 }
 
 export default App;
